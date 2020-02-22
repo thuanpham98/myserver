ad419772059b9b63cee933b88135b45b828bef63
Thuan <pmttmp24@gmail.com>
2020-02-22 12:12:34 +0700

    update 22/2/2019

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
+//--add module--///
 const express = require('express');
 const app = express();
 
-
+//--config port--///
 myPort = 6969;
 
+//--- config direction of router--///
 var index = require('./routers/index');
-var users = require('./routers/users');
+var user = require('./routers/users');
+var viewer = require('./routers/viewers');
+var creator = require('./routers/creators');
 
 
+//--set view engine--//
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', './views'); // view folder if equaltion with app.js and public folder
+
+//--use router and static--//
 app.use(express.static('public'));
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', user);
+app.use('/viewer', viewer);
+app.use('/creator', creator);
 
 
 
-
-
+//---- listen--///
 app.listen(myPort, function() {
     console.log("listening");
 });
