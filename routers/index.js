d624d6795a0ff74913fe4ed36ea342124bcc2e70
Thuan <pmttmp24@gmail.com>
2020-02-25 00:59:39 +0700

    update 25/2/2020

--- routers/index.js
+++ routers/index.js
@@ -2,11 +2,34 @@ var express = require('express');
 var router = express.Router();
 var user_account = [];
 
+var popup = require('popups');
+var assert = require('assert');
+
+var client = require('../models/database');
+
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
 });
+
+// //---midleware to authority url you want-----//
+// function requireLogin(req, res, next) {
+//     if (req.session.loggedIn) {
+//         next(); // allow the next route to run
+//     } else {
+//         // require the user to log in
+//         res.redirect("/access"); // or render a form, etc.
+//     }
+// }
+
+// // Automatically apply the `requireLogin` middleware to all
+// // routes starting with `/admin`
+// router.all("/user/*", requireLogin, function(req, res, next) {
+//     next(); // if the middleware allowed us to get here,
+//     // just move on to the next route handler
+// });
+//------------------------------------------///
 
 //----Home page ---///
 router.get('/', function(req, res) {
@@ -20,7 +43,6 @@ router.get('/access', function(req, res) {
 });
 
 router.post('/access', function(req, res) {
-    console.log(user_account[user_account.lenth - 1] + "acessing");
     res.redirect('/user');
 });
 //-------------------------//
@@ -31,17 +53,26 @@ router.get('/asign', function(req, res) {
 });
 
 router.post('/asign', function(req, res) {
-    let account = {
-        id: user_account.length,
-        name: req.body.a_name,
-        pass: req.body.a_pass
-    }
-    user_account.push(account);
-    console.log(user_account[user_account.length - 1]);
-    res.redirect('/asign/te');
-});
-router.get('/asign/te', function(req, res) {
-    res.send(user_account);
+    let i_account = {
+            //_id: Date.now(),
+            name: req.body.a_name,
+            pass: req.body.a_pass
+        }
+        //user_account.push(i_account);
+    client.connect(function(err) {
+        assert.equal(null, err);
+        var account = client.db('account');
+        account.collection("user_account").insertOne(i_account, function(err, res) {
+            assert.equal(null, err);
+            console.log("your ID is " + res.insertedId);
+            popup.alert({
+                content: "your ID is " + res.insertedId
+            });
+
+        });
+    });
+
+    res.redirect('/access');
 });
 //-------------------------//
 
