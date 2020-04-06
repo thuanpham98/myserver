081d559a63d95ed90308bffeaca64b944279d96d
Thuan <pmttmp24@gmail.com>
2020-04-06 12:51:38 +0700

    update model

--- models/email.js
+++ models/email.js
@@ -2,9 +2,14 @@ var nodemailer = require('nodemailer');
 
 module.exports.mailServer = nodemailer.createTransport({
     service: 'gmail',
+    secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASS_EMAIL
+    },
+    tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
     }
 });
 module.exports.form = {
