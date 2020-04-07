cecc947c413b4e46a5ad81550813d9571da673b5
Thuan <pmttmp24@gmail.com>
2020-04-08 00:58:24 +0700

    update

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -2,139 +2,156 @@
 var mychart = document.getElementById("mychart").getContext('2d');
 var mychart2 = document.getElementById("mychart2").getContext('2d');
 
-var massPopChart = new Chart(mychart, {
-    type:'line', 
-    data:{
-        labels: [],
-        datasets:[{
-            fill:false,
-            label:"Temperature",
-            data: [],
-            backgroundColor:'#FC0000',
-            borderColor:'#FC0000',
-            borderWidth: 2,
-            hidden: true
-        },{
-            fill:false,
-            label:"Humidity",
-            data: [],
-            backgroundColor:'#1f78b4',
-            borderColor:'#1f78b4',
-            borderWidth: 2,
-            hidden: true
-        },{
-            fill:false,
-            label:"Humidity",
-            data: [],
-            backgroundColor:'#4dfd00',
-            borderColor:'#4dfd00',
-            borderWidth: 2,
-            hidden: true
-        },{
-            fill:false,
-            label:"Humidity",
-            data: [],
-            backgroundColor:'#220ffd',
-            borderColor:'#220ffd',
-            borderWidth: 2,
-            hidden: true
-        },{
-            fill:false,
-            label:"Humidity",
-            data: [],
-            backgroundColor:'#ff9dcb',
-            borderColor:'#ff9dcb',
-            borderWidth: 2,
-            hidden: true
-        },{
-            fill:false,
-            label:"Humidity",
-            data: [],
-            backgroundColor:'#f7e92d',
-            borderColor:'#f7e92d',
-            borderWidth: 2,
-            hidden: true
-        },{
-            fill:false,
-            label:"Humidity",
-            data: [],
-            backgroundColor:'#fa9041',
-            borderColor:'#fa9041',
-            borderWidth: 2,
-            hidden: true
-        },{
-            fill:false,
-            label:"Humidity",
-            data: [],
-            backgroundColor:'#050904',
-            borderColor:'#050904',
-            borderWidth: 2,
-            hidden: true
-        },{
-            fill:false,
-            label:"Humidity",
-            data: [],
-            backgroundColor:'#6d6f04',
-            borderColor:'#6d6f04',
-            borderWidth: 2,
-            hidden: true
-        },{
-            fill:false,
-            label:"Humidity",
-            data: [],
-            backgroundColor:'#a0522d',
-            borderColor:'#a0522d',
-            borderWidth: 2,
-            hidden: true
-        }
-        ]
-    },
-    options:{
-        title:{
-            display:true,
-            text:'Chart flow time',
-            fontSize:25
-        },
-        legend:{
-            display:true,
-            position:'right',
-            labels:{
-                fontColor:'	#000000'
-
+function makeChart(){
+    return  new Chart(mychart, {
+        type:'line', 
+        data:{
+            labels: [],
+            datasets:[{
+                fill:false,
+                label:"Temperature",
+                data: [],
+                backgroundColor:'#FC0000',
+                borderColor:'#FC0000',
+                borderWidth: 2,
+                hidden: true
+            },{
+                fill:false,
+                label:"Humidity",
+                data: [],
+                backgroundColor:'#1f78b4',
+                borderColor:'#1f78b4',
+                borderWidth: 2,
+                hidden: true
+            },{
+                fill:false,
+                label:"Humidity",
+                data: [],
+                backgroundColor:'#4dfd00',
+                borderColor:'#4dfd00',
+                borderWidth: 2,
+                hidden: true
+            },{
+                fill:false,
+                label:"Humidity",
+                data: [],
+                backgroundColor:'#220ffd',
+                borderColor:'#220ffd',
+                borderWidth: 2,
+                hidden: true
+            },{
+                fill:false,
+                label:"Humidity",
+                data: [],
+                backgroundColor:'#ff9dcb',
+                borderColor:'#ff9dcb',
+                borderWidth: 2,
+                hidden: true
+            },{
+                fill:false,
+                label:"Humidity",
+                data: [],
+                backgroundColor:'#f7e92d',
+                borderColor:'#f7e92d',
+                borderWidth: 2,
+                hidden: true
+            },{
+                fill:false,
+                label:"Humidity",
+                data: [],
+                backgroundColor:'#fa9041',
+                borderColor:'#fa9041',
+                borderWidth: 2,
+                hidden: true
+            },{
+                fill:false,
+                label:"Humidity",
+                data: [],
+                backgroundColor:'#050904',
+                borderColor:'#050904',
+                borderWidth: 2,
+                hidden: true
+            },{
+                fill:false,
+                label:"Humidity",
+                data: [],
+                backgroundColor:'#6d6f04',
+                borderColor:'#6d6f04',
+                borderWidth: 2,
+                hidden: true
+            },{
+                fill:false,
+                label:"Humidity",
+                data: [],
+                backgroundColor:'#a0522d',
+                borderColor:'#a0522d',
+                borderWidth: 2,
+                hidden: true
             }
+            ]
         },
-        layout:{
-            padding:{
-                left:50,
-                right:0,
-                bottom:0,
-                top:0
+        options:{
+            title:{
+                display:true,
+                text:'Chart flow time',
+                fontSize:25
+            },
+            legend:{
+                display:true,
+                position:'right',
+                labels:{
+                    fontColor:'	#000000'
+    
+                }
+            },
+            layout:{
+                padding:{
+                    left:50,
+                    right:0,
+                    bottom:0,
+                    top:0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                            display: true,
+                            labelString: 'Time',
+                            fontSize : 40
+                            
+                    },
+                    ticks: {
+                        min: 0,
+                        max: 10,
+                  
+                        // forces step size to be 5 units
+                        stepSize: 1 // <----- This prop sets the stepSize
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                            display: true,
+                            labelString: 'Value (%)',
+                            fontSize : 40
+                    },
+                    ticks: {
+                        min: 0,
+                        max: 100,
+                  
+                        // forces step size to be 5 units
+                        stepSize: 1 // <----- This prop sets the stepSize
+                    }
+                }]
+            },
+            tooltips:{
+                enabled:true
             }
-        },
-        scales: {
-            xAxes: [{
-                display: true,
-                scaleLabel: {
-                        display: true,
-                        labelString: 'Time',
-                        fontSize: 30
-                }
-            }],
-            yAxes: [{
-                display: true,
-                scaleLabel: {
-                        display: true,
-                        labelString: 'Value (%)',
-                        fontSize: 30
-                }
-            }]
-        },
-        tooltips:{
-            enabled:true
         }
-    }
-}); 
-
+    });
+} 
+var massPopChart = makeChart();
 var massPopChart2 = new Chart(mychart2, {
     type:'bar', 
     data:{
@@ -250,7 +267,7 @@ var massPopChart2 = new Chart(mychart2, {
                 scaleLabel: {
                         display: true,
                         labelString: 'Time',
-                        fontSize: 30
+                        fontSize : 40
                 }
             }],
             yAxes: [{
@@ -258,7 +275,14 @@ var massPopChart2 = new Chart(mychart2, {
                 scaleLabel: {
                         display: true,
                         labelString: 'Value (%)',
-                        fontSize: 30
+                        fontSize : 40
+                },
+                ticks: {
+                    min: 0,
+                    max: 100,
+              
+                    // forces step size to be 5 units
+                    stepSize: 1 // <----- This prop sets the stepSize
                 }
             }]
         },
@@ -268,8 +292,9 @@ var massPopChart2 = new Chart(mychart2, {
     }
 }); 
 //--this post id drives the example data
+var k=0;
 async function getData(){
-    var response= await fetch('https://iotmakerserver.herokuapp.com/user/display/getdata');
+    var response= await fetch('http://localhost:6969/user/display/getdata');
     var datum= await response.text();
     datum =JSON.parse(datum);
 
@@ -280,6 +305,7 @@ async function getData(){
         i ++;
     });
     massPopChart.update();
+    
 
     i=0;
     massPopChart2.data.labels=[datum.label];
@@ -288,5 +314,23 @@ async function getData(){
         i ++;
     });
     massPopChart2.update();
+    k=k+1;
 }
-setInterval(getData, 2000);
+function removeData(){
+    if(k===10){
+        let j=0;
+
+        massPopChart.clear();
+
+        massPopChart.data.labels=[];
+        massPopChart.data.datasets.forEach((dataset) => {
+            dataset.data=[];
+            j++;
+        });
+        massPopChart.update();
+        k=0;
+    }
+}
+
+setInterval(getData, 1000);
+setInterval(removeData, 1000);
