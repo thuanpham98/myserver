ad95a40d46456f68a8eb54c7042d2f7ebd801f83
Thuận <pmttmp24@gmail.com>
2020-04-14 23:52:52 +0700

    update

--- app.js
+++ app.js
@@ -61,9 +61,9 @@ app.use('/user/gui', gui);
 
 /** redict https and http because android >=8 use https  */
 app.use(function (req, res, next) {
-    if (!req.secure) {
+    if (req.secure) {
         // request was via https, so do no special handling
-        res.redirect('https://' + req.headers.host + req.url);
+        res.redirect('http://' + req.headers.host + req.url);
         console.log(req.headers.host);
         console.log(req.url);
         //next();
