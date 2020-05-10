778b929878368a90e628582c9ad5c13e8e0c82ce
Thuận <pmttmp24@gmail.com>
2020-05-10 13:48:07 +0700

    update

--- public/javascripts/datatables-demo.js
+++ public/javascripts/datatables-demo.js
@@ -1,9 +1,36 @@
 // Call the dataTables jQuery plugin
+
+var data=[
+  {
+      "name":       "Tiger Nixon",
+      "position":   "System Architect",
+      "salary":     "$3,120",
+      "start_date": "2011/04/25",
+      "office":     "Edinburgh",
+      "extn":       "5421"
+  },
+  {
+      "name":       "Garrett Winters",
+      "position":   "Director",
+      "salary":     "$5,300",
+      "start_date": "2011/07/25",
+      "office":     "Edinburgh",
+      "extn":       "8422"
+  }
+];
+
 $(document).ready(function () {
   $('#dataTable').DataTable({
-    // dom: 'Bfrtip',
-    // buttons: [
-    //   'copy', 'csv', 'excel', 'pdf', 'print'
-    // ]
+    dom: 'Bfrtip',
+    buttons: [
+      'copy', 'csv', 'excel', 'pdf', 'print'
+    ],
+    data: data,
+    columns: [
+        { data: 'name' },
+        { data: 'position' },
+        { data: 'salary' },
+        { data: 'office' }
+    ]
   });
 });
