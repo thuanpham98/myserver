27dc9fb4ed3143aa9ac33355444d6b033962d41a
Thuận <pmttmp24@gmail.com>
2020-05-10 13:15:01 +0700

    update

--- public/javascripts/datatables-demo.js
+++ public/javascripts/datatables-demo.js
@@ -1,4 +1,9 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
+$(document).ready(function () {
+  $('#dataTable').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+  });
 });
