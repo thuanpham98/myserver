0e5199067d9d1384c05259cc84d22bf95f735f21
Thuận <pmttmp24@gmail.com>
2020-05-12 16:21:14 +0700

    update

--- public/javascripts/chart-line.js
+++ public/javascripts/chart-line.js
@@ -4,7 +4,7 @@ Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSyste
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
 var temp_dataset = [];
-var temp_color = ['#FC0000', '#1f78b4', '#4dfd00', '#220ffd', '#ff9dcb', '#f7e92d', '#fa9041', '#050904', '#6d6f04', '#a0522d'];
+var temp_color = ['#FC0000', '#1f78b4', '#4dfd00', '#220ffd', '#ff9dcb', '#f7e92d', '#fa9041', '#050904', '#6d6f04', '#a0522d','#FC0000', '#1f78b4', '#4dfd00', '#220ffd', '#ff9dcb', '#f7e92d', '#fa9041', '#050904', '#6d6f04', '#a0522d'];
 async function init_data() {
     let mess = { message: "init" };
     mess = JSON.stringify(mess);
