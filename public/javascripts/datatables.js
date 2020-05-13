56421909368daa464ab576909558215de7cf7ad7
Thuận <pmttmp24@gmail.com>
2020-05-13 15:01:55 +0700

    update

--- public/javascripts/datatables.js
+++ public/javascripts/datatables.js
@@ -3,48 +3,8 @@
 // Call the dataTables jQuery plugin
 
 var temp_mask = [];
-var data = [
-  {
-    "name": "temperature",
-    "value": "23",
-    "datetime": "2011/04/25",
-    "timestamp": "5421"
-  },
-  {
-    "name": "humanality",
-    "value": "12",
-    "datetime": "2011/07/25",
-    "timestamp": "8422"
-  },
-  {
-    "name": "báo cháy",
-    "value": "123",
-    "datetime": "2011/07/25",
-    "timestamp": "8422"
-  }
-];
-
-$(document).ready(function () {
-  $('#dataTable').DataTable({
-    dom: 'Bfrtip',
-    buttons: {
-      buttons: [
-        { extend: 'copy', className: 'copyButton' },
-        { extend: 'excel', className: 'excelButton' },
-        { extend: 'csv', className: 'csvButton' },
-        { extend: 'pdf', className: 'pdfButton' },
-        { extend: 'print', className: 'printButton' }
-      ]
-    },
-    data: data,
-    columns: [
-      { data: 'name' },
-      { data: 'value' },
-      { data: 'datetime' },
-      { data: 'timestamp' }
-    ]
-  });
-});
+var data_frame={name : "thuan",value : "22", datetime: "1998/10/05",timestamp: "907567107"};
+var dataTables = [];
 
 async function init_data_table() {
   let mess_table = { message: "init" };
@@ -61,13 +21,38 @@ async function init_data_table() {
 
   for (let i = 0; i < datum.init.length; i++) {
       temp_mask.push(datum.init[i].mask);
+      data_frame.name=datum.init[i].mask;
+      dataTables.push(JSON.stringify(data_frame));
   }
-  console.log(temp_mask);
 }
 init_data_table();
 
+$(document).ready(function () {
+  $('#dataTable').DataTable({
+    dom: 'Bfrtip',
+    buttons: {
+      buttons: [
+        { extend: 'copy', className: 'copyButton' },
+        { extend: 'excel', className: 'excelButton' },
+        { extend: 'csv', className: 'csvButton' },
+        { extend: 'pdf', className: 'pdfButton' },
+        { extend: 'print', className: 'printButton' }
+      ]
+    },
+    data: dataTables,
+    columns: [
+      { data: 'name' },
+      { data: 'value' },
+      { data: 'datetime' },
+      { data: 'timestamp' }
+    ]
+  });
+});
 
-// async function getData_bar() {
+
+
+
+// async function getData_table() {
 //     let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
 //         method: 'get',
 //         mode: 'cors',
@@ -79,14 +64,6 @@ init_data_table();
 
 //     let datum = await response.json();
 //     console.log(datum.data);
-
-//     let i = 0;
-//     massbarChart.data.labels = [datum.label];
-//     massbarChart.data.datasets.forEach((dataset) => {
-//         dataset.data = [datum.data[temp_index_bar[i]]];
-//         i++;
-//     });
-//     massbarChart.update();
 // }
 
-// setInterval(getData_bar, 1000);
+// setInterval(getData_table, 1000);
