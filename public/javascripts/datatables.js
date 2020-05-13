f2d30bb066514e6ff372f91732628495ca0b5ef9
Thuận <pmttmp24@gmail.com>
2020-05-13 15:38:10 +0700

    update

--- public/javascripts/datatables.js
+++ public/javascripts/datatables.js
@@ -6,32 +6,32 @@ var temp_mask = [];
 var data_frame=[];
 var dataTables = [];
 
-async function init_data_table() {
-  let mess_table = { message: "init" };
-  mess_table = JSON.stringify(mess_table);
-  let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
-    method: "POST",
-    body: mess_table,
-    mode: "cors",
-    headers: { "Content-type": "application/json;charset=utf-8" }
-  });
-  let datum = await response.json();
+// async function init_data_table() {
+//   let mess_table = { message: "init" };
+//   mess_table = JSON.stringify(mess_table);
+//   let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
+//     method: "POST",
+//     body: mess_table,
+//     mode: "cors",
+//     headers: { "Content-type": "application/json;charset=utf-8" }
+//   });
+//   let datum = await response.json();
 
   
 
-  for (let i = 0; i < datum.init.length; i++) {
-      temp_mask.push(datum.init[i].mask);
-      data_frame.push(datum.init[i].mask);
-      data_frame.push("29");
-      data_frame.push("today");
-      data_frame.push("172893");
-      dataTables.push((data_frame));
-      data_frame=[];
-  }
-  console.log(dataTables);
-}
-init_data_table();
-dataTables=["mask1","29","today","172893"];
+//   for (let i = 0; i < datum.init.length; i++) {
+//       temp_mask.push(datum.init[i].mask);
+//       data_frame.push(datum.init[i].mask);
+//       data_frame.push("29");
+//       data_frame.push("today");
+//       data_frame.push("172893");
+//       dataTables.push((data_frame));
+//       data_frame=[];
+//   }
+//   console.log(dataTables);
+// }
+// init_data_table();
+// dataTables=["mask1","29","today","172893"];
 
 function init_table(){
   $(document).ready(function () {
