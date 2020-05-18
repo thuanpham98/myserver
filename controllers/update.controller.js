3fde02654f1f517a20a5e20ca5cb58d161c538d5
thuan <pmttmp24@gmail.com>
2020-05-18 09:41:46 +0700

    update

--- controllers/update.controller.js
+++ controllers/update.controller.js
@@ -5,10 +5,6 @@ var bcrypt = require('bcrypt');
 /* modal User */
 var User = require('../models/user');
 
-/* module email to check ID */
-var Email = require('../models/email');
-var nodemailer = require('nodemailer');
-
 //---------------------------------------------------------------------//
 module.exports.get = function(req, res) {
     res.render('update', {
