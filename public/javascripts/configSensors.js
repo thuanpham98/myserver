24f7fe5fa3b8a37d6c80d8819df32e0639c1098a
thuan <pmttmp24@gmail.com>
2020-05-19 13:40:52 +0700

    update

---
+++ public/javascripts/configSensors.js
@@ -0,0 +1,41 @@
+"use strict"
+
+var frame,email,pass ;
+
+async function postData(){
+
+    email =document.getElementById("email").value;
+    pass =document.getElementById("pass").value;
+    
+    email=email.toString();
+    pass=pass.toString();
+    frame={email : email , pass : pass};
+
+    let response = await fetch('https://iot-server-365.herokuapp.com/login',{
+        method: 'post',
+        mode: 'cors', 
+        headers:{
+            //'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(frame)
+    });
+    
+    let datum= await response;
+    
+    if(datum.redirected)
+    {
+        window.location.replace(datum.url);
+    }
+    else{
+        //window.location.replace( "/login");
+        console.log("error");
+        document.getElementById("status").innerHTML="email or pass is incorrect";
+        
+    }
+}
+
+document.getElementById("login").addEventListener("click", function(){
+    console.log("button");
+    postData();
+});
