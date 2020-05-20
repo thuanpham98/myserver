456e1ab3bb8d3a075b58ed3a715cf1e4ba500f22
thuan <pmttmp24@gmail.com>
2020-05-20 14:31:45 +0700

    update

--- routers/config.js
+++ routers/config.js
@@ -20,15 +20,21 @@ router.get('/sensors',function(req,res){
     let device =[{mask: "thuan", type : 0},{mask:"thao",type : 1}]
     res.render('sensors', { title: "Sensor Page" , dev: device});
 });
-
-router.get('/equipments',function(req,res){
-    let device =[{mask: "thuan", type : 0},{mask:"thao",type : 1}]
-    res.render('sensors', { title: "Sensor Page" , dev: device});
-})
-
 router.get('/sensors/:id',function(req,res){
     res.json({id : req.params.id});
 });
+
+/** euipments config */
+router.get('/equipments',function(req,res){
+    let device =[{mask: "thuan", type : 0},{mask:"thao",type : 1}]
+    res.render('equipments', { title: "Sensor Page" , dev: device});
+})
+router.get('/equipments/:id',function(req,res){
+    let device =[{mask: "thuan", type : 0},{mask:"thao",type : 1}]
+    res.json({id : req.params.id});
+})
+
+
 
 /* export user/GUI */ 
 module.exports = router
