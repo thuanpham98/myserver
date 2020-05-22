f3cddbd7a3cbba0e7e3331b17fa72ed59bb96d4b
thuan <pmttmp24@gmail.com>
2020-05-23 05:20:19 +0700

    update

--- routers/config.js
+++ routers/config.js
@@ -136,10 +136,12 @@ router.post('/sensors', async function (req, res) {
     let ind = parseInt(frame.child.index, 10);
     let dev_num = parseInt(frame.dev, 10);
     console.log(frame.action);
+
     if (parseInt(frame.action, 10) === 0) {
         console.log("status 0");
         await ManageDev.find({ ID: account[0].timestamp, dev: dev_num}, function (err, result) {
-            result[0].child[ind].act = JSON.parse(frame.child.status);
+            console.log(result[0].child[ind]);
+            result[0].child[ind].act = frame.child.status;
             result[0].save();
         });
     }
