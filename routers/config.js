8063b255ce82cf39ea589385658a3dc7f0d533fb
thuan <pmttmp24@gmail.com>
2020-05-23 16:19:23 +0700

    update

--- routers/config.js
+++ routers/config.js
@@ -40,7 +40,7 @@ router.post('/', async function (req, res) {
     });
 
     if (req.body.act) {
-        await ManageDev.find({ ID: account[0].timestamp, dev: parseInt(req.body.dev,10) }, async function (errr, result) {
+        await ManageDev.find({ ID: account[0].timestamp, dev: parseInt(req.body.dev, 10) }, async function (errr, result) {
             // assert.equal(null, err);
             device = result;
 
@@ -49,35 +49,28 @@ router.post('/', async function (req, res) {
                 result[0].dev = req.body.dev;
                 result[0].mask = req.body.mask;
                 result[0].type = req.body.type;
-                if(req.body.num > result[0].child.length){
-                    // let child =result[0].child;
-                    for(let i =result[0].child.length ; i < req.body.num ;i++){
-                        // let ind = frame.child[i].index;
-                        // child.push({ mask: ("mask"+i.toString()), type: 0, act: false });
-                        // result[0].child.set(i,child[i]);
-                        console.log(i);
-                        result[0].child.push({ mask: ("mask"+i.toString()), type: 0, act: false });
+                if (req.body.num > result[0].child.length) {
+                    for (let i = result[0].child.length; i < req.body.num; i++) {
+                        result[0].child.push({ mask: ("mask" + i.toString()), type: 0, act: false });
                     }
                     result[0].save();
                 }
-                else if(req.body.num < result[0].child.length){
-                    // let child =result[0].child;
-                    for(let i =result[0].child.length ; i > req.body.num ;i--){
+                else if (req.body.num < result[0].child.length) {
+                    for (let i = result[0].child.length; i > req.body.num; i--) {
 
                         result[0].child.pop();
                     }
                     result[0].save();
                 }
-                
+
             }
             else {
-                if(req.body.type){
+                if (parseInt(req.body.type, 10) === 1) {
 
-                    let child = [{ mask: "mask1", type: 0, act: false }, { mask: "mask2", type: 0, act: false }, { mask: "mask3", type: 0, act: false }, { mask: "mask4", type: 0, act: false }, { mask: "mask5", type: 0, act: false },
-                    { mask: "mask6", type: 0, act: false }, { mask: "mask7", type: 0, act: false }, { mask: "mask8", type: 0, act: false }, { mask: "mask9", type: 0, act: false }, { mask: "mask10", type: 0, act: false },
-                    { mask: "mask11", type: 1, act: false }, { mask: "mask12", type: 1, act: false }, { mask: "mask13", type: 1, act: false }, { mask: "mask14", type: 1, act: false }, { mask: "mask15", type: 1, act: false },
-                    { mask: "mask16", type: 1, act: false }, { mask: "mask17", type: 1, act: false }, { mask: "mask18", type: 1, act: false }, { mask: "mask19", type: 1, act: false }, { mask: "mask20", type: 1, act: false }
-                    ];
+                    let child = [];
+                    for (let i = 0; i < parseInt(req.body.num, 10); i++) {
+                        child.push({ mask: ("mask" + i.toString()), type: 0, act: false });
+                    }
                     await ManageDev.create({
                         ID: account[0].timestamp,
                         timestamp: Date.now(),
@@ -89,19 +82,12 @@ router.post('/', async function (req, res) {
                         console.log(doc);
                     });
                 }
-                else {
+                else if (parseInt(req.body.type, 10) === 0) {
 
-                    let child = [{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},
-                                {mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},
-                                {mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},
-                                {mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},
-                                {mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},
-                                {mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},
-                                {mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},
-                                {mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},
-                                {mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},
-                                {mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false},{mask: "mask1",type : 0 , value : 0 , pin : 0,act :false}                
-                    ];
+                    let child = [];
+                    for (let i = 0; i < parseInt(req.body.num, 10); i++) {
+                        child.push({ mask: ("mask" + i.toString()), maskport: "maskPortx", type: 0, value: 0, port: 0, pin: i, act: false });
+                    }
                     await ManageDev.create({
                         ID: account[0].timestamp,
                         timestamp: Date.now(),
@@ -113,7 +99,7 @@ router.post('/', async function (req, res) {
                         console.log(doc);
                     });
                 }
-                
+
             }
         });
         res.json("added");
@@ -176,77 +162,77 @@ router.post('/sensors', async function (req, res) {
     let frame = req.body;
     let dev_num = parseInt(frame.dev, 10);
 
-    switch (parseInt(frame.action,10)) {
+    switch (parseInt(frame.action, 10)) {
         case 0:
-            
-            await ManageDev.find({ ID: account[0].timestamp, dev: dev_num}, async function (err, result) {
-            
+
+            await ManageDev.find({ ID: account[0].timestamp, dev: dev_num }, async function (err, result) {
+
                 let ind = parseInt(frame.child.index, 10);
-                let child =result[0].child[ind];
+                let child = result[0].child[ind];
                 child.act = frame.child.status;
-                result[0].child.set(ind,child);
+                result[0].child.set(ind, child);
                 await result[0].save();
             });
             break;
 
-        case 1 : 
-            
-            await ManageDev.find({ ID: account[0].timestamp, dev: dev_num}, async function (err, result) {
-                
+        case 1:
+
+            await ManageDev.find({ ID: account[0].timestamp, dev: dev_num }, async function (err, result) {
+
                 let ind = parseInt(frame.child.index, 10);
-                let child =result[0].child[ind];
+                let child = result[0].child[ind];
                 child.type = frame.child.type;
-                result[0].child.set(ind,child);
+                result[0].child.set(ind, child);
                 await result[0].save();
             });
             break;
 
-        case 2: 
-        
-            await ManageDev.find({ ID: account[0].timestamp, dev: dev_num}, async function (err, result) {
-                
-                let child =result[0].child;
-                for(let i =0;i < frame.child.length;i++){
+        case 2:
+
+            await ManageDev.find({ ID: account[0].timestamp, dev: dev_num }, async function (err, result) {
+
+                let child = result[0].child;
+                for (let i = 0; i < frame.child.length; i++) {
                     let ind = frame.child[i].index;
-                    child[ind].mask= frame.child[i].mask;
-                    result[0].child.set(ind,child[ind]);
+                    child[ind].mask = frame.child[i].mask;
+                    result[0].child.set(ind, child[ind]);
                 }
                 await result[0].save();
             });
             break;
-    
+
         default:
             break;
     }
     res.json({ name: "ok user" });
 });
-router.post('/sensors/search',async function(req,res){
-        // let device =[{mask: "thuan", type : 0},{mask:"thao",type : 1}]
-        let account, sensors;
-        let decoded = await jwt.verify(req.cookies.access_token, process.env.PRIVATE_KEY);
-    
-        /* check token on database */
-        await User.find({ email: decoded.accessToken }, function (err, result) {
-            assert.equal(null, err);
-            account = result;
-    
-            if (!account.length) {
-                res.json("no user");
-                return;
-            }
-        });
-        /** check device of user */
-        let frame = req.body;
-        await ManageDev.find({ ID: account[0].timestamp, mask: frame.mask }, function (err, result) {
-            sensors = result;
-            if(sensors.length){
+router.post('/sensors/search', async function (req, res) {
+    // let device =[{mask: "thuan", type : 0},{mask:"thao",type : 1}]
+    let account, sensors;
+    let decoded = await jwt.verify(req.cookies.access_token, process.env.PRIVATE_KEY);
 
-                res.json({pathDev: sensors[0].dev.toString()})
-            }
-            else{
-                res.json({pathDev : ''});
-            }
-        });
+    /* check token on database */
+    await User.find({ email: decoded.accessToken }, function (err, result) {
+        assert.equal(null, err);
+        account = result;
+
+        if (!account.length) {
+            res.json("no user");
+            return;
+        }
+    });
+    /** check device of user */
+    let frame = req.body;
+    await ManageDev.find({ ID: account[0].timestamp, mask: frame.mask }, function (err, result) {
+        sensors = result;
+        if (sensors.length) {
+
+            res.json({ pathDev: sensors[0].dev.toString() })
+        }
+        else {
+            res.json({ pathDev: '' });
+        }
+    });
 })
 router.get('/sensors/:id', async function (req, res) {
     //res.json({id : req.params.id});
@@ -268,15 +254,31 @@ router.get('/sensors/:id', async function (req, res) {
     let devi = parseInt(req.params.id, 10);
     await ManageDev.find({ ID: account[0].timestamp, dev: devi }, function (err, result) {
         sensors = result;
-        res.render('sens', { title: "Sensi Page", name: sensors[0].mask, sens: sensors[0].child, dev: sensors[0].dev , numSensi : sensors[0].child.length});
+        res.render('sens', { title: "Sensi Page", name: sensors[0].mask, sens: sensors[0].child, dev: sensors[0].dev, numSensi: sensors[0].child.length });
     });
 });
 
 
 /** euipments config */
 router.get('/equipments', function (req, res) {
-    let device = [{ mask: "thuan", type: 0 }, { mask: "thao", type: 1 }]
-    res.render('equipments', { title: "Sensor Page", dev: device });
+        // let device =[{mask: "thuan", type : 0},{mask:"thao",type : 1}]
+        let account, device;
+        let decoded = await jwt.verify(req.cookies.access_token, process.env.PRIVATE_KEY);
+    
+        /* check token on database */
+        await User.find({ email: decoded.accessToken }, function (err, result) {
+            assert.equal(null, err);
+            account = result;
+    
+            if (!account.length) {
+                res.json("no user");
+                return;
+            }
+        });
+        await ManageDev.find({ ID: account[0].timestamp, type: 0 }, function (errr, result) {
+            device = result;
+            res.render('equipments', { title: "Equipment Page", dev: device });
+        });
 })
 router.get('/equipments/:id', function (req, res) {
     let device = [{ mask: "thuan", type: 0 }, { mask: "thao", type: 1 }]
