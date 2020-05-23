485730636370d7cfbf3a9210dddcbf5ed21be9e1
thuan <pmttmp24@gmail.com>
2020-05-23 17:07:06 +0700

    update

--- routers/config.js
+++ routers/config.js
@@ -62,8 +62,6 @@ router.post('/', async function (req, res) {
                         }
                         result[0].save();
                     }
-
-                    
                 }
                 else if (req.body.num < result[0].child.length) {
                     for (let i = result[0].child.length; i > req.body.num; i--) {
@@ -291,8 +289,27 @@ router.get('/equipments', async function (req, res) {
         });
 })
 router.get('/equipments/:id', function (req, res) {
-    let device = [{ mask: "thuan", type: 0 }, { mask: "thao", type: 1 }]
-    res.json({ id: req.params.id });
+        //res.json({id : req.params.id});
+        let account, sensors;
+        let decoded = await jwt.verify(req.cookies.access_token, process.env.PRIVATE_KEY);
+    
+        /* check token on database */
+        await User.find({ email: decoded.accessToken }, function (err, result) {
+            assert.equal(null, err);
+            account = result;
+    
+            if (!account.length) {
+                res.json("no user");
+                return;
+            }
+        });
+    
+        /** check device of user */
+        let devi = parseInt(req.params.id, 10);
+        await ManageDev.find({ ID: account[0].timestamp, dev: devi }, function (err, result) {
+            equipments = result;
+            res.render('equips', { title: "Equipi Page", name: equipments[0].mask, equipi: equipments[0].child, dev: equipments[0].dev, numEquipi: equipments[0].child.length });
+        });
 })
 
 
