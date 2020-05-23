ef47e470d380d729f88f92f9a4d787b0e4dfd1ed
thuan <pmttmp24@gmail.com>
2020-05-23 15:04:54 +0700

    update

--- public/javascripts/configDev.js
+++ public/javascripts/configDev.js
@@ -1,58 +1,49 @@
 "use strict"
 
-var frame,dev,mask,type,act=-1;
+var frame, dev, mask, type, act = -1;
 
-async function postData(){
+async function postData() {
 
-    dev =document.getElementById("dev").value;
-    mask =document.getElementById("mask").value;
+    dev = document.getElementById("dev").value;
+    mask = document.getElementById("mask").value;
     type = document.getElementById("type").value;
+    num = document.getElementById("num").value;
 
-    dev=dev.toString();
-    mask=mask.toString();
-    type=type.toString();
+    dev = dev.toString();
+    mask = mask.toString();
+    type = type.toString();
+    num = num.toString();
 
-    if((dev!=="") && (mask !=="")&&(type !=="")){
-        frame={dev : dev  ,mask: mask , type : type,act : act};
+    if ((dev !== "") && (mask !== "") && (type !== "") && (num !== "")) {
+        frame = { dev: dev, mask: mask, type: type, num: num, act: act };
         console.log(frame);
-        let response = await fetch('https://iot-server-365.herokuapp.com/user/config',{
+        let response = await fetch('https://iot-server-365.herokuapp.com/user/config', {
             method: 'post',
-            mode: 'cors', 
-            headers:{
+            mode: 'cors',
+            headers: {
                 //'Accept': 'application/json, text/plain, */*',
                 'Content-Type': 'application/json'
             },
-            body:JSON.stringify(frame)
+            body: JSON.stringify(frame)
         });
-        
-        let datum= await response.json();
 
-    window.location.replace('/user/config');
-    window.alert(datum);
+        let datum = await response.json();
+
+        window.location.replace('/user/config');
+        window.alert(datum);
     }
-    else{
-        document.getElementById("status").innerHTML="fill all the parameter";
+    else {
+        document.getElementById("status").innerHTML = "fill all the parameter";
     }
-    
-    // if(datum.redirected)
-    // {
-    //     window.location.replace(datum.url);
-    // }
-    // else{
-    //     //window.location.replace( "/login");
-    //     console.log("error");
-    //     document.getElementById("status").innerHTML="email or pass is incorrect";
-        
-    // }
 }
 
-document.getElementById("addDev").addEventListener("click", function(){
-    act=1;
+document.getElementById("addDev").addEventListener("click", function () {
+    act = 1;
     console.log(act);
     postData();
 });
-document.getElementById("removeDev").addEventListener("click", function(){
-    act=0;
+document.getElementById("removeDev").addEventListener("click", function () {
+    act = 0;
     console.log(act);
     postData();
 });
