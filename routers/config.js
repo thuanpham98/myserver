2d6e4b68dc189b5369480e5279a462ff69ddf699
thuan <pmttmp24@gmail.com>
2020-05-25 17:15:17 +0700

    update

--- routers/config.js
+++ routers/config.js
@@ -394,8 +394,8 @@ router.post('/equipments', async function (req, res) {
         default:
             break;
     }
-    
-    await ManageDev.find({ ID: account[0].timestamp, dev: dev_num }, async function (err, doc) {
+
+    await ManageDev.find({ ID: account[0].timestamp, 'child.type': 1}, async function (err, doc) {
         let ret = doc;
         if(ret.length){
             for(let i =0; i < ret.length;i++){
