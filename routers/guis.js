e1ccb1bbf9bc8e96a23c9e077fcdab3d678d7010
thuan <pmttmp24@gmail.com>
2020-06-01 23:29:44 +0700

    update

--- routers/guis.js
+++ routers/guis.js
@@ -290,7 +290,7 @@ router.get('/blocks/:id', async function (req, res) {
     let indexBlock = req.params.id;
     console.log(indexBlock);
 
-    await ManageDev.find({ ID: account[0].timestamp,child: { port : indexBlock} }, function (errr, result) {
+    await ManageDev.find({ ID: account[0].timestamp,type : 0 , 'child.port' : -1}, function (errr, result) {
         // equipments = result;
         // res.render('equips', { title: "Equipi Page", name: equipments[0].mask, equips: equipments[0].child, dev: equipments[0].dev, numEquipi: equipments[0].child.length });
 
