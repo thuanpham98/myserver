252a04d4d5807b47130dda88cce664e617fcf01d
thuan <pmttmp24@gmail.com>
2020-06-03 23:44:10 +0700

    update

--- routers/guis.js
+++ routers/guis.js
@@ -13,6 +13,9 @@ var ManageDev = require('../models/manageDev');
 /* modal  user */
 var User = require('../models/user');
 
+/** modal command */
+var Command = require('../models/command');
+
 /* module  check error */
 var assert = require('assert');
 
@@ -97,7 +100,7 @@ router.post('/', async function (req, res) {
                         doc[0].child.set(ind, child[ind]);
                     }
                     /** syn mask again */
-                    for (let i = 0; i < index_used.length ; i++) {
+                    for (let i = 0; i < index_used.length; i++) {
                         let ind = index_used[i];
 
                         child[ind].port = frame.block;
@@ -125,7 +128,7 @@ router.post('/', async function (req, res) {
                         doc[0].child.set(ind, child[ind]);
                     }
                     /** syn mask */
-                    for (let i = 0; i < index_used.length ; i++) {
+                    for (let i = 0; i < index_used.length; i++) {
                         let ind = index_used[i];
 
                         child[ind].port = frame.block;
@@ -160,18 +163,18 @@ router.post('/', async function (req, res) {
     // Free Block 
     else if (frame.act === 0) {
         console.log(frame.block);
-        console.log(typeof(frame.block))
+        console.log(typeof (frame.block))
 
         await ManageDev.find({ ID: account[0].timestamp, dev: parseInt(frame.dev, 10) }, function (err, result) {
             let child = result[0].child;
-            
+
             for (let i = 0; i < child.length; i++) {
-                if (child[i].port === parseInt(frame.block,10) ) {
+                if (child[i].port === parseInt(frame.block, 10)) {
                     child[i].maskport = "maskPort";
                     child[i].port = -1;
                     console.log(child[i].port);
                 }
-                
+
                 result[0].child.set(i, child[i]);
             }
             result[0].save();
@@ -201,14 +204,14 @@ router.get('/blocks', async function (req, res) {
             return;
         }
     });
-    await ManageDev.find({ ID: account[0].timestamp, type : 0 }, function (errr, result) {
+    await ManageDev.find({ ID: account[0].timestamp, type: 0 }, function (errr, result) {
         device = result;
-        let block=[];
-        let name_block=[];
-        if(device.length){
-            for(let i =0 ; i < device.length; i++){
-                for(let j =0 ; j < device[i].child.length;j++){
-                    if(device[i].child[j].port !== -1){
+        let block = [];
+        let name_block = [];
+        if (device.length) {
+            for (let i = 0; i < device.length; i++) {
+                for (let j = 0; j < device[i].child.length; j++) {
+                    if (device[i].child[j].port !== -1) {
                         block.push(device[i].child[j].port);
                         name_block.push(device[i].child[j].maskport);
 
@@ -216,25 +219,25 @@ router.get('/blocks', async function (req, res) {
                 }
             }
         }
-        
-        let arr_block  = block.reduce(function(obj,item){
-            obj[item]= (obj[item] || 0) +1;
-            return obj;
-        },{});
-        name_block = name_block.reduce(function(obj,item){
-            obj[item]= (obj[item] || 0) +1;
-            return obj;
-        },{});
 
-        let item_block=Object.keys(arr_block);
+        let arr_block = block.reduce(function (obj, item) {
+            obj[item] = (obj[item] || 0) + 1;
+            return obj;
+        }, {});
+        name_block = name_block.reduce(function (obj, item) {
+            obj[item] = (obj[item] || 0) + 1;
+            return obj;
+        }, {});
+
+        let item_block = Object.keys(arr_block);
         let number_block = Object.values(arr_block);
         name_block = Object.keys(name_block);
 
-        res.render('blocks', { title: "Block Page", block_item : item_block , block_number : number_block, block_name : name_block});
+        res.render('blocks', { title: "Block Page", block_item: item_block, block_number: number_block, block_name: name_block });
     });
 });
-router.post('/blocks', async function (req, res){
-    let account, comma,device;
+router.post('/blocks', async function (req, res) {
+    let account, comma;
     let decoded = await jwt.verify(req.cookies.access_token, process.env.PRIVATE_KEY);
 
     /* check token on database */
@@ -243,32 +246,43 @@ router.post('/blocks', async function (req, res){
         account = result;
 
         if (!account.length) {
-            res.json("no user");
+            res.json({ status: "no user" });
             return;
         }
     });
     /** check device of user */
     let frame = req.body;
 
-    await ManageDev.find({ ID: account[0].timestamp, dev: parseInt(frame.dev, 10) ,type : 0}, async function (err, result) {
-        if(result.length){
+    await ManageDev.find({ ID: account[0].timestamp, dev: parseInt(frame.dev, 10), type: 0 }, async function (err, result) {
+        if (result.length) {
             console.log(result[0].child);
-            let child =result[0].child;
-            for(let i =0; i < child.length;i++){
-                if((child[i].port == frame.port) && (child[i].pin == frame.pin)){
+            let child = result[0].child;
+            for (let i = 0; i < child.length; i++) {
+                if ((child[i].port == frame.port) && (child[i].pin == frame.pin)) {
 
-                    child[i].value=parseInt(frame.value,10);
-                    result[0].child.set(i,child[i]);
+                    child[i].value = parseInt(frame.value, 10);
+                    result[0].child.set(i, child[i]);
                     console.log("fined it");
                 }
             }
             await result[0].save();
-            res.send({status:"ok"});
+            res.send({ status: "ok" });
         }
         else {
-            res.send({status:"no pin"});
+            res.send({ status: "no pin" });
         }
     });
+
+    comma = {
+        ID: account[0].timestamp,
+        timestamp: new Date().toLocaleString('en-US', { timeZone: process.env.TIME_ZONE }),
+        device: parseInt(frame.dev, 10),
+        io: parseInt(frame.pin, 10),
+        value: parseInt(frame.value, 10),
+    };
+    let ret = await Command.create(comma);
+    console.log(ret);
+
 });
 router.post('/blocks/search', async function (req, res) {
 
@@ -288,19 +302,19 @@ router.post('/blocks/search', async function (req, res) {
     /** check device of user */
     let frame = req.body;
 
-    await ManageDev.find({ ID: account[0].timestamp, type : 0 ,"child.maskport": frame.mask }, function (err, result) {
+    await ManageDev.find({ ID: account[0].timestamp, type: 0, "child.maskport": frame.mask }, function (err, result) {
         blocks = result;
         let res_search;
         if (blocks.length) {
 
-            for(let i =0; i < blocks[0].child.length;i++){
-                if(blocks[0].child[i].maskport == frame.mask){
+            for (let i = 0; i < blocks[0].child.length; i++) {
+                if (blocks[0].child[i].maskport == frame.mask) {
                     res_search = blocks[0].child[i].port;
                     console.log(res_search);
                     break
                 }
             }
-            res.json({ pathDev: res_search})
+            res.json({ pathDev: res_search })
         }
         else {
             res.json({ pathDev: '' });
@@ -309,7 +323,7 @@ router.post('/blocks/search', async function (req, res) {
 });
 router.get('/blocks/:id', async function (req, res) {
 
-    let account,blocks, blockManager=[];
+    let account, blocks, blockManager = [];
     let decoded = await jwt.verify(req.cookies.access_token, process.env.PRIVATE_KEY);
 
     /* check token on database */
@@ -325,21 +339,21 @@ router.get('/blocks/:id', async function (req, res) {
 
     /** check Block of user */
     let indexBlock = req.params.id;
-    await ManageDev.find({ ID: account[0].timestamp,type : 0 , 'child.port' : indexBlock}, function (errr, result) {
+    await ManageDev.find({ ID: account[0].timestamp, type: 0, 'child.port': indexBlock }, function (errr, result) {
         // equipments = result;
         // res.render('equips', { title: "Equipi Page", name: equipments[0].mask, equips: equipments[0].child, dev: equipments[0].dev, numEquipi: equipments[0].child.length });
 
         blocks = result;
-        if(blocks.length){
-            for( let i =0 ; i < blocks.length;i++){
-                for(let j = 0  ; j < blocks[i].child.length; j++){
-                    if(blocks[i].child[j].port === indexBlock){
-                        blockManager.push({dev: blocks[i].dev, mask : blocks[i].mask , block : blocks[i].child[j]});
+        if (blocks.length) {
+            for (let i = 0; i < blocks.length; i++) {
+                for (let j = 0; j < blocks[i].child.length; j++) {
+                    if (blocks[i].child[j].port === indexBlock) {
+                        blockManager.push({ dev: blocks[i].dev, mask: blocks[i].mask, block: blocks[i].child[j] });
                     }
                 }
             }
         }
-        res.render('blocki', { title: "Control Block Page", name: blockManager[0].block.maskport,port : indexBlock, blocks: blockManager});
+        res.render('blocki', { title: "Control Block Page", name: blockManager[0].block.maskport, port: indexBlock, blocks: blockManager });
     });
 })
 /* export home */
