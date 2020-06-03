33a993ae8288fd40e761007cf60b2cde64d66515
thuan <pmttmp24@gmail.com>
2020-06-03 23:30:55 +0700

    update

--- routers/guis.js
+++ routers/guis.js
@@ -254,15 +254,15 @@ router.post('/blocks', async function (req, res){
         if(result.length){
             console.log(result[0].child);
             let child =result[0].child;
-            for(let i =0; i < result[0].child.length;i++){
-                if((child[i].port === parseInt(frame.port,10)) && (child[i].pin === parseInt(frame.pin,10))){
+            for(let i =0; i < child.length;i++){
+                if((child[i].port == frame.port) && (child[i].pin == frame.pin)){
 
-                    child[i].value=parseInt(frame.value,10);
-                    result[0].child.set(i,child[i]);
+                    // child[i].value=parseInt(frame.value,10);
+                    // result[0].child.set(i,child[i]);
                     console.log("fined it");
                 }
             }
-            await result[0].save();
+            // await result[0].save();
             res.send({status:"ok"});
         }
         else {
