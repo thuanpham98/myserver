2661dc73888fe6ca5b40e0aa7415b1e0e406de4e
thuan <pmttmp24@gmail.com>
2020-06-03 22:16:03 +0700

    update

--- public/javascripts/controlBlock_Digi.js
+++ public/javascripts/controlBlock_Digi.js
@@ -1,39 +1,41 @@
 "use strict"
 
-var frame,block_name,block_index,pin_index,pin_val;
+var frame,dev_id ,dev, block_name, block_index, pin_index, pin_val;
 
-async function portBlockCommand_Digi(){
+async function portBlockCommand_Digi() {
 
-    block_index=document.getElementById("numBlock").innerHTML;
-    block_name= document.getElementById("blockName").innerHTML;
+    block_index = document.getElementById("numBlock").innerHTML;
+    block_name = document.getElementById("blockName").innerHTML;
+    dev = document.getElementById(dev_id).innerHTML;
 
-    frame={port : block_index , maskport : block_name,pin : pin_index, value : pin_val};
+    frame = { dev: dev, port: block_index, maskport: block_name, pin: pin_index, value: pin_val };
     console.log(frame);
-    let response = await fetch('https://iot-server-365.herokuapp.com/user/gui/blocks',{
+    let response = await fetch('https://iot-server-365.herokuapp.com/user/gui/blocks', {
         method: 'post',
-        mode: 'cors', 
-        headers:{
+        mode: 'cors',
+        headers: {
             //'Accept': 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         },
-        body:JSON.stringify(frame)
+        body: JSON.stringify(frame)
     });
 
-    let datum= await response.json();
+    let datum = await response.json();
     console.log(datum.status);
 }
 
 $(document).ready(function () {
 
-    $(".blocki_digi").change(function (){
+    $(".blocki_digi").change(function () {
         let id = $(this).attr("id");
+        dev_id = 'd'+id;
 
-        pin_index=parseInt(id.slice(1));
-        if($(this).prop('checked')){
-            pin_val=1;
+        pin_index = parseInt(id.slice(1));
+        if ($(this).prop('checked')) {
+            pin_val = 1;
         }
         else {
-            pin_val=0;
+            pin_val = 0;
         }
         portBlockCommand_Digi();
         console.log(pin_index + "is" + pin_val.toString());
