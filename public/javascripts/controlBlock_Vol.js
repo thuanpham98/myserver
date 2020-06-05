31b0a60b503b14fecc3176e45a32f3041a858c42
thuan <pmttmp24@gmail.com>
2020-06-06 02:26:35 +0700

    update

--- public/javascripts/controlBlock_Vol.js
+++ public/javascripts/controlBlock_Vol.js
@@ -59,9 +59,7 @@ async function portBlockCommand_Digi() {
 let a =document.getElementsByClassName("slider");
 console.log(a);
 
-let b = a[0].document.addEventListener("onchange",function(e){
-    console.log(e);
-});
+let b = document.addEventListener("input");
 
 console.log(b);
 // function showSliderValue() {
