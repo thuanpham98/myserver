6dbcae25c16c7f6829508c28dd488c67eff5be43
thuan <pmttmp24@gmail.com>
2020-06-08 03:02:38 +0700

    update

--- middlewares/data.validate.js
+++ middlewares/data.validate.js
@@ -11,23 +11,32 @@ module.exports.checkID = async function(req, res, next) {
     let time = new Date().toLocaleString('en-US', { timeZone: process.env.TIME_ZONE });//Date().toString();
     console.log(req.body.ID);
     console.log("start check");
-    await User.find({ timestamp: req.body.ID }, function(err, result) {
+    if((req.body.ID.length ==undefined) || (req.body.ID.length !=13) ||(isNaN(req.body.ID.length))){\
+        res.send("who are you");
+        return;
+    }
+
+    await User.find({ timestamp: req.body.ID }, async function(err, result) {
         assert.equal(null, err);
         if (!result.length) {
+            res.send("who are you");
             return;
+        }
+        else{
+            let ob = {
+                ID: req.body.ID,
+                device : req.body.dev,
+                datetime : req.body.datetime,
+                timestamp: req.body.timestamp,
+                form: req.body.form
+            };
+        
+            console.log("start send");
+            let doc = await Data.create(ob);
+            console.log(doc);
+            next();
         }
     });
 
-    let ob = {
-        ID: req.body.ID,
-        device : req.body.dev,
-        datetime : req.body.datetime,
-        timestamp: req.body.timestamp,
-        form: req.body.form
-    };
 
-    console.log("start send");
-    let result = await Data.create(ob);
-    console.log(result);
-    next();
 };
