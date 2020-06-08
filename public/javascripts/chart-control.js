9655c5b892350aec8cd960ee49b4636abe1fc94d
thuan <pmttmp24@gmail.com>
2020-06-08 23:37:17 +0700

    update

--- public/javascripts/chart-control.js
+++ public/javascripts/chart-control.js
@@ -3,7 +3,7 @@ document.getElementById("choosen").addEventListener("click", function(){
     init_data_bar();
     init_data_line();
 
-    // setInterval(getData_line, 1000);
-    // setInterval(removeData, 5000);
-    // setInterval(getData_bar, 1000);
+    setInterval(getData_line, 1000);
+    setInterval(removeData, 5000);
+    setInterval(getData_bar, 1000);
 });
