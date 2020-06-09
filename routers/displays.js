ae60b6cd08ecd4b0723613cd05135c3f79440be4
thuan <pmttmp24@gmail.com>
2020-06-09 16:56:02 +0700

    update

--- routers/displays.js
+++ routers/displays.js
@@ -73,7 +73,12 @@ router.get('/getdata', async function (req, res) {
                     data = result;
                     let m_label = data[0].datetime;
                     console.log(data[0].form);
-                    let m_data = data[0].form//[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+                    let m_data = [];
+                    for(let i = 0 ; i < data[0].form.length;i++){
+                        let temp_data="sensor_"+(i+1).toString();
+                        m_data.push(data[0].form[temp_data]);
+                    }
+                    //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
                     console.log(m_data);
                     let resAPI = { label: m_label, data: m_data };
                     console.log(resAPI);
