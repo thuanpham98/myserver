72454bed81fd757fbdaeb639a1c854c9de5e9ed7
thuan <pmttmp24@gmail.com>
2020-06-11 20:33:24 +0700

    update

--- public/javascripts/datatables.js
+++ public/javascripts/datatables.js
@@ -2,103 +2,113 @@
 
 // Call the dataTables jQuery plugin
 
-var dev;
-var temp_mask = [];
-var data_frame =  {
-  "name": "temperature",
-  "value": "23",
-  "datetime": "2011/04/25"
-};
+// var dev;
+// var temp_mask = [];
+// var data_frame =  {
+//   "name": "temperature",
+//   "value": "23",
+//   "datetime": "2011/04/25"
+// };
 
-var store ={
-  retrieve: true,
-  dom: 'Bfrtip',
-  buttons: {
-    buttons: [
-      { extend: 'copy', className: 'copyButton' },
-      { extend: 'excel', className: 'excelButton' },
-      { extend: 'csv', className: 'csvButton' },
-      { extend: 'pdf', className: 'pdfButton' },
-      { extend: 'print', className: 'printButton' }
-    ]
-  },
-  data: [],
-  columns: [
-    { data: 'name' },
-    { data: 'value' },
-    { data: 'datetime' }
-  ]
-};
+// var store ={
+//   retrieve: true,
+//   dom: 'Bfrtip',
+//   buttons: {
+//     buttons: [
+//       { extend: 'copy', className: 'copyButton' },
+//       { extend: 'excel', className: 'excelButton' },
+//       { extend: 'csv', className: 'csvButton' },
+//       { extend: 'pdf', className: 'pdfButton' },
+//       { extend: 'print', className: 'printButton' }
+//     ]
+//   },
+//   data: [],
+//   columns: [
+//     { data: 'name' },
+//     { data: 'value' },
+//     { data: 'datetime' }
+//   ]
+// };
 
-$(document).ready(function () {
-  $('#dataTable').DataTable(store);
-});
+// $(document).ready(function () {
+//   $('#dataTable').DataTable(store);
+// });
 
-//----------------------------------------------------------------------------------------
+// //----------------------------------------------------------------------------------------
 
 
-async function init_data_table() {
+// async function init_data_table() {
 
-  dev=document.getElementById("tables").value;
+//   dev=document.getElementById("tables").value;
 
-  let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
-    method: "POST",
-    body: JSON.stringify({dev: dev}),
-    mode: "cors",
-    headers: { "Content-type": "application/json;charset=utf-8" }
-  });
-  let datum = await response.json();
+//   let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
+//     method: "POST",
+//     body: JSON.stringify({dev: dev}),
+//     mode: "cors",
+//     headers: { "Content-type": "application/json;charset=utf-8" }
+//   });
+//   let datum = await response.json();
 
-  if (datum.init == null) {
-    return;
-  }
-  console.log(datum.init);
+//   if (datum.init == null) {
+//     return;
+//   }
+//   console.log(datum.init);
 
-  let dataTable=new Array ( datum.init.length);
-  for (let i = 0; i < datum.init.length; i++) {
-    temp_mask.push(datum.init[i].mask);
-    data_frame.name=datum.init[i].mask;
-    dataTable[i]=data_frame;
-    $('#dataTable').dataTable().fnAddData(dataTable[i]); 
-  }
+//   let dataTable=new Array ( datum.init.length);
+//   for (let i = 0; i < datum.init.length; i++) {
+//     temp_mask.push(datum.init[i].mask);
+//     data_frame.name=datum.init[i].mask;
+//     dataTable[i]=data_frame;
+//     $('#dataTable').dataTable().fnAddData(dataTable[i]); 
+//   }
   
-}
+// }
 
-async function getData_table() {
-  $('#dataTable').dataTable().fnClearTable();
-    let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
-        method: 'get',
-        mode: 'cors',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-            id:dev
-        }
-    });
+// async function getData_table() {
+//   $('#dataTable').dataTable().fnClearTable();
+//     let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
+//         method: 'get',
+//         mode: 'cors',
+//         headers: {
+//             'Accept': 'application/json, text/plain, */*',
+//             'Content-Type': 'application/json',
+//             id:dev
+//         }
+//     });
 
-    let datum = await response.json();
+//     let datum = await response.json();
 
-    if (datum === null) {
-      return;
+//     if (datum === null) {
+//       return;
+//     }
+//     console.log(datum.data);
+//     let dataTable=new Array ( datum.data.length);
+//     for (let i = 0; i < datum.data.length; i++) {
+//       data_frame.name=temp_mask[i];
+//       data_frame.value = datum.data[i].toString();
+//       data_frame.datetime = datum.label;
+//       dataTable[i]=data_frame;
+//       $('#dataTable').dataTable().fnAddData(dataTable[i]);
+//     }
+// }
+
+// document.getElementById("add_table").addEventListener("click", function () {
+//   init_data_table();
+//   getData_table();
+// });
+
+// document.getElementById("clear_table").addEventListener("click", function () {
+//   $('#dataTable').dataTable().fnClearTable();
+// });
+
+
+$(document).ready(function(){
+  $("#list-header").on({
+    mouseenter : function(){
+      $(this).css("background-color","blue");
+    },
+    mouseleave: function(){
+      $(this).css("background-color","lightblue");
     }
-    console.log(datum.data);
-    let dataTable=new Array ( datum.data.length);
-    for (let i = 0; i < datum.data.length; i++) {
-      data_frame.name=temp_mask[i];
-      data_frame.value = datum.data[i].toString();
-      data_frame.datetime = datum.label;
-      dataTable[i]=data_frame;
-      $('#dataTable').dataTable().fnAddData(dataTable[i]);
-    }
-}
-
-document.getElementById("add_table").addEventListener("click", function () {
-  init_data_table();
-  getData_table();
-});
-
-document.getElementById("clear_table").addEventListener("click", function () {
-  $('#dataTable').dataTable().fnClearTable();
-});
-
-
+  })
+})
