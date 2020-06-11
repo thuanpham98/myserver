08e008702e37605b51109646a473fa553425679a
thuan <pmttmp24@gmail.com>
2020-06-12 01:13:17 +0700

    update

--- public/javascripts/datatables.js
+++ public/javascripts/datatables.js
@@ -7,13 +7,14 @@ var num;
 async function getData_table() {
   dev = document.getElementById("tables").value;
   num =document.getElementById("number").value;
+  console.log(num);
   if(num=""){
     num="1";
   }
   else if(isNaN(num)){
     return;
   }
-  console.log(num);
+  
   dataForm=[];
     let response = await fetch('https://iot-server-365.herokuapp.com/user/display/datatable', {
         method: 'get',
