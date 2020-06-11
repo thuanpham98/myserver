c9f36837fc152acc3682ee9e8f38e9ce6be71922
thuan <pmttmp24@gmail.com>
2020-06-11 21:09:48 +0700

    update

--- public/javascripts/datatables.js
+++ public/javascripts/datatables.js
@@ -1,14 +1,15 @@
 "use strict"
-
+var table;
+var dataForm;
 // Call the dataTables jQuery plugin
 
-// var dev;
-// var temp_mask = [];
-// var data_frame =  {
-//   "name": "temperature",
-//   "value": "23",
-//   "datetime": "2011/04/25"
-// };
+var dev;
+var temp_mask = [];
+var data_frame =  {
+  "name": "temperature",
+  "value": "23",
+  "datetime": "2011/04/25"
+};
 
 // var store ={
 //   retrieve: true,
@@ -39,7 +40,7 @@
 
 // async function init_data_table() {
 
-//   dev=document.getElementById("tables").value;
+//   dev = document.getElementById("tables").value;
 
 //   let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
 //     method: "POST",
@@ -63,7 +64,6 @@
 //   }
 
 // }
-
 // async function getData_table() {
 //   $('#dataTable').dataTable().fnClearTable();
 //     let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
@@ -92,6 +92,65 @@
 //     }
 // }
 
+async function init_data_table() {
+
+  dev = document.getElementById("tables").value;
+
+  let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
+    method: "POST",
+    body: JSON.stringify({dev: dev}),
+    mode: "cors",
+    headers: { "Content-type": "application/json;charset=utf-8" }
+  });
+  let datum = await response.json();
+
+  if (datum.init == null) {
+    return;
+  }
+  console.log(datum.init);
+
+  let data_Table=new Array ( datum.init.length);
+  for (let i = 0; i < datum.init.length; i++) {
+    temp_mask.push(datum.init[i].mask);
+    data_frame.name=datum.init[i].mask;
+    dataTable[i]=data_frame;
+     
+  }
+  dataForm=data_Table;
+
+}
+
+async function getData_table() {
+  $('#dataTable').dataTable().fnClearTable();
+    let response = await fetch('https://iot-server-365.herokuapp.com/user/display/getdata', {
+        method: 'get',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+            id:dev
+        }
+    });
+
+    let datum = await response.json();
+
+    if (datum === null) {
+      return;
+    }
+    console.log(datum.data);
+
+    let dataTable=new Array ( datum.data.length);
+    for (let i = 0; i < datum.data.length; i++) {
+      // data_frame.name=temp_mask[i];
+      // data_frame.value = datum.data[i].toString();
+      // data_frame.datetime = datum.label;
+      // dataTable[i]=data_frame;
+      dataTable[i].name= temp_mask[i];
+      dataTable[i].value= datum.data[i].toString();
+      dataTable[i].datetime = datum.label;
+    }
+}
+
 // document.getElementById("add_table").addEventListener("click", function () {
 //   init_data_table();
 //   getData_table();
@@ -102,9 +161,10 @@
 // });
 
 
-var table;
 function initTable() {
-  var data = [
+  init_data_table();
+  getData_table();
+  dataForm = [
     {
       "name": "temperature",
       "value": "21",
