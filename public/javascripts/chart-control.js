f97b8184c76e13ac466f863b7fc69e49fe289031
thuan <pmttmp24@gmail.com>
2020-07-02 16:08:24 +0700

    update

--- public/javascripts/chart-control.js
+++ public/javascripts/chart-control.js
@@ -2,6 +2,10 @@ var dev;
 var start=0; /** khong cho auto chay */
 init_data_bar();
 init_data_line();
+start=1;
+getData_line();
+getData_bar();
+start=0;
 document.getElementById("choosen").addEventListener("click", function(){
     // console.log("button");
     start=0;
